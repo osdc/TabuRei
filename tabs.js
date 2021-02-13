17407b6b9651b97f3326a3a7794a36c52e8893ba
Aayush Kumar Sahu <aayushmau5@gmail.com>
2021-02-13 23:40:58 +0530

    Add save tabs functionality (#1)

--- tabs.js
+++ tabs.js
@@ -1,4 +1,5 @@
 let tabList = [];
+let store;
 
 function getCurrentWindowTabs() {
   return browser.tabs.query({ currentWindow: true });
@@ -8,7 +9,6 @@ function listTabs() {
   getCurrentWindowTabs().then((tabs) => {
     let tabsList = document.getElementById("tabs-list");
     let currentTabs = document.createDocumentFragment();
-
     for (let tab of tabs) {
       let tabElement = document.createElement("li");
       let tabLink = document.createElement("a");
@@ -20,6 +20,20 @@ function listTabs() {
 
     tabsList.appendChild(currentTabs);
   });
+}
+
+function storeTabs(tabs) {
+  const storedTabs = tabs.map((tab) => ({
+    id: tab.id,
+    title: tab.title,
+    url: tab.url,
+  }));
+
+  store = {
+    storedTabs,
+  };
+
+  return browser.storage.local.set(store);
 }
 
 document.addEventListener("DOMContentLoaded", listTabs);
@@ -46,4 +60,17 @@ document.getElementById("remove").addEventListener("click", function () {
 
     tabsList.appendChild(currentTabs);
   });
+
+const saveBtn = document.querySelector(".saveBtn");
+saveBtn.addEventListener("click", () => {
+  getCurrentWindowTabs()
+    .then((tabs) => {
+      return storeTabs(tabs);
+    })
+    .then(() => {
+      return browser.storage.local.get(store);
+    })
+    .then((result) => {
+      result.storedTabs.forEach((value) => console.log(value));
+    });
 });
