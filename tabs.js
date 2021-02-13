2a207d9eb92ea523ceae664e2dbeb340b961c933
daemon1024 <barun1024@gmail.com>
2021-02-13 23:31:10 +0530

    collapse tab on button click

--- tabs.js
+++ tabs.js
@@ -1,3 +1,5 @@
+let tabList = [];
+
 function getCurrentWindowTabs() {
   return browser.tabs.query({ currentWindow: true });
 }
@@ -9,7 +11,10 @@ function listTabs() {
 
     for (let tab of tabs) {
       let tabElement = document.createElement("li");
-      tabElement.textContent = tab.title;
+      let tabLink = document.createElement("a");
+      tabLink.href = tab.url;
+      tabLink.innerText = tab.title;
+      tabElement.appendChild(tabLink);
       currentTabs.appendChild(tabElement);
     }
 
@@ -18,3 +23,27 @@ function listTabs() {
 }
 
 document.addEventListener("DOMContentLoaded", listTabs);
+document.getElementById("remove").addEventListener("click", function () {
+  getCurrentWindowTabs().then((tabs) => {
+    function onSuccess() {
+      console.log(`Success`);
+    }
+
+    function onError(error) {
+      console.log(`Error: ${error}`);
+    }
+
+    var creating = browser.tabs.create({
+      url: "index.html",
+    });
+
+    creating.then(onSuccess, onError);
+
+    for (let tab of tabs) {
+      var removing = browser.tabs.remove(tab.id);
+      removing.then(onSuccess, onError);
+    }
+
+    tabsList.appendChild(currentTabs);
+  });
+});
