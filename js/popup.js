bcd5551a8f01d88552c00475cf812041945578ae
daemon1024 <barun1024@gmail.com>
2021-02-20 23:02:53 +0530

    use time in microseconds instead of uuid

--- js/popup.js
+++ js/popup.js
@@ -21,19 +21,6 @@ function listTabs() {
   });
 }
 
-function generate_UUID() {
-  var dt = new Date().getTime();
-  var uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
-    /[xy]/g,
-    function (c) {
-      var r = (dt + Math.random() * 16) % 16 | 0;
-      dt = Math.floor(dt / 16);
-      return (c == "x" ? r : (r & 0x3) | 0x8).toString(16);
-    }
-  );
-  return uuid;
-}
-
 function storeTabs(tabs) {
   const validTabs = tabs.filter((tab) => !tab.url.startsWith("about:"));
 
@@ -58,7 +45,7 @@ function storeTabs(tabs) {
     };
   });
 
-  let groupId = generate_UUID();
+  let groupId = performance.timeOrigin + performance.now();
 
   let store = {
     [groupId]: storedTabs,
