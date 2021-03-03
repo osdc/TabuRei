7bed81b81c49374091d7ad7c2eaa31a880ef752f
Aayush Kumar Sahu <aayushmau5@gmail.com>
2021-03-03 21:29:42 +0530

    add option to selectively collapse tabs (#24)
    * Add option to not collapse certain tabs
    
    * Add 'collapse all' and 'collpase unselected' buttons
    
    * Add conditional for "collapse unselected" button

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,5 @@
+let blacklist = [];
+
 function getCurrentWindowTabs() {
   return browser.tabs.query({ currentWindow: true });
 }
@@ -8,16 +10,26 @@ function listTabs() {
     let currentTabs = document.createDocumentFragment();
     for (let tab of tabs) {
       if (!tab.url.startsWith("about:")) {
-        let tabElement = document.createElement("li");
-        let bulletPoint = document.createElement("button");
-        bulletPoint.className = "popup";
-        bulletPoint.innerHTML = "&#9726";
-        bulletPoint.disabled = true;
-        tabElement.appendChild(bulletPoint);
-        let tabLink = document.createElement("a");
-        tabLink.href = tab.url;
-        tabLink.innerText = tab.title;
-        tabElement.appendChild(tabLink);
+        let tabElement = document.createElement("label");
+        tabElement.classList.add("popup-element-container");
+        let tabTitle = document.createTextNode(tab.title);
+        tabElement.appendChild(tabTitle);
+        tabElement.setAttribute("tab-id", tab.id);
+        let checkbox = document.createElement("input");
+        checkbox.setAttribute("type", "checkbox");
+        checkbox.addEventListener("click", removeTab);
+        tabElement.appendChild(checkbox);
+        let checkmark = document.createElement("span");
+        checkmark.classList.add("checkmark");
+        tabElement.appendChild(checkmark);
+        // let bulletPoint = document.createElement("button");
+        // bulletPoint.className = "popup";
+        // bulletPoint.innerHTML = "&#9726";
+        // bulletPoint.disabled = true;
+        // tabElement.appendChild(bulletPoint);
+        // let tabLink = document.createElement("a");
+        // tabLink.href = tab.url;
+        // tabElement.appendChild(tabLink);
         currentTabs.appendChild(tabElement);
       }
     }
@@ -26,8 +38,10 @@ function listTabs() {
   });
 }
 
-function storeTabs(tabs) {
-  const validTabs = tabs.filter((tab) => !tab.url.startsWith("about:"));
+function storeTabs(tabs, blacklistedTabs) {
+  const validTabs = tabs.filter(
+    (tab) => !tab.url.startsWith("about:") && !blacklistedTabs.includes(tab.id)
+  );
 
   let allowedProperties = [
     "url",
@@ -59,10 +73,9 @@ function storeTabs(tabs) {
   return browser.storage.local.set(store);
 }
 
-document.addEventListener("DOMContentLoaded", listTabs);
-document.getElementById("collapse").addEventListener("click", function () {
+function onBtnClick(blackedlistedArray) {
   getCurrentWindowTabs().then((tabs) => {
-    let storing = storeTabs(tabs);
+    let storing = storeTabs(tabs, blackedlistedArray);
     Promise.resolve(storing);
 
     let creating = browser.tabs.create({
@@ -72,11 +85,23 @@ document.getElementById("collapse").addEventListener("click", function () {
     Promise.resolve(creating);
 
     for (let tab of tabs) {
-      let removing = browser.tabs.remove(tab.id);
-      Promise.resolve(removing);
+      if (!blackedlistedArray.includes(tab.id)) {
+        let removing = browser.tabs.remove(tab.id);
+        Promise.resolve(removing);
+      }
     }
   });
-});
+}
+
+document.addEventListener("DOMContentLoaded", listTabs);
+
+document
+  .getElementById("collapse")
+  .addEventListener("click", () => onBtnClick([]));
+
+document
+  .getElementById("collapse-unselected")
+  .addEventListener("click", () => onBtnClick(blacklist));
 
 document.getElementById("tab-manager").addEventListener("click", function () {
   let creating = browser.tabs.create({
@@ -84,3 +109,20 @@ document.getElementById("tab-manager").addEventListener("click", function () {
   });
   Promise.resolve(creating);
 });
+
+const removeTab = (e) => {
+  const parent = e.target.parentElement;
+  const tabId = +parent.getAttribute("tab-id");
+  const conditionalBtn = document.getElementById("collapse-unselected");
+  if (e.target.checked) {
+    blacklist.push(tabId);
+    parent.classList.add("crossed");
+    conditionalBtn.classList.remove("hidden-btn");
+  } else {
+    blacklist = blacklist.filter((id) => id != tabId);
+    parent.classList.remove("crossed");
+    if (blacklist.length === 0) {
+      conditionalBtn.classList.add("hidden-btn");
+    }
+  }
+};
